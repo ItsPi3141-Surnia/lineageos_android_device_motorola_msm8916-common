aab91af08406bcd2856005f2b4ac16e0c19b25fd
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-11-07 14:20:36 +0000

    msm8916-common: Set ro.vndk.version to current

--- system_prop.mk
+++ system_prop.mk
@@ -137,7 +137,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # System
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.binary_xml=false
+    persist.sys.binary_xml=false \
+    ro.vndk.version=current
 
 # Timeout failed shutdowns
 PRODUCT_PROPERTY_OVERRIDES += \
