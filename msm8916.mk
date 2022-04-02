2884a7e86f59d6ac3cb159481c7b10d051614830
althafvly <althafvly@gmail.com>
2022-04-02 10:16:59 +0530

    msm8916-common: build MotoActions

--- msm8916.mk
+++ msm8916.mk
@@ -224,6 +224,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+ifeq ($(filter harpia,$(TARGET_DEVICE)),)
+PRODUCT_PACKAGES += \
+    MotoActions
+endif
+
 # Netmgrd
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
